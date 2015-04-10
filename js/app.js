ec88069d7a734373b10620571cf9052479b07706
dickinsonmr1 <dickinsonmr1@gmail.com>
2015-04-09 22:52:59 -0400

    playing around with spritesheets

--- js/app.js
+++ js/app.js
@@ -1,11 +1,14 @@
-﻿var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+﻿/// <reference path="phaser.js" />
+
+var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
 
 function preload() {
 
     game.load.image('sky', 'assets/sky.png');
     game.load.image('ground', 'assets/platform.png');
     game.load.image('star', 'assets/star.png');
-    game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
+    //game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
+    game.load.atlasXML('player1', 'assets/player/spritesheet_players.png', 'assets/player/spritesheet_players.xml');
 
 }
 
@@ -33,7 +36,7 @@ function create() {
     var ground = platforms.create(0, game.world.height - 64, 'ground');
 
     //  Scale it to fit the width of the game (the original sprite is 400x32 in size)
-    ground.scale.setTo(2, 2);
+    ground.scale.setTo(3, 2);
 
     //  This stops it from falling away when you jump on it
     ground.body.immovable = true;
@@ -46,19 +49,24 @@ function create() {
     ledge.body.immovable = true;
 
     // The player and its settings
-    player = game.add.sprite(32, game.world.height - 150, 'dude');
+    player = game.add.sprite(32, game.world.height - 250, 'player1', 'alienBlue_front.png');
+    player.scale.setTo(0.5, 0.5);
+    player.anchor.setTo(.5, .5);
+    player.animations.add('walk', Phaser.Animation.generateFrameNames('alienBeige_walk', 1, 2, '.png'), 10);
+    //player.animations.play('walk', 2, true);
+    
 
     //  We need to enable physics on the player
     game.physics.arcade.enable(player);
 
     //  Player physics properties. Give the little guy a slight bounce.
     player.body.bounce.y = 0.2;
-    player.body.gravity.y = 300;
+    player.body.gravity.y = 500;
     player.body.collideWorldBounds = true;
 
     //  Our two animations, walking left and right.
-    player.animations.add('left', [0, 1, 2, 3], 10, true);
-    player.animations.add('right', [5, 6, 7, 8], 10, true);
+    //player.animations.add('left', [0, 1, 2, 3], 10, true);
+    //player.animations.add('right', [5, 6, 7, 8], 10, true);
 
     //  Finally some stars to collect
     stars = game.add.group();
@@ -98,20 +106,24 @@ function update() {
     if (cursors.left.isDown) {
         //  Move to the left
         player.body.velocity.x = -150;
+        player.anchor.setTo(.5, .5);
+        player.scale.x *= -1;
 
-        player.animations.play('left');
+        player.animations.play('walk');
+        //player.animations.play('left');
     }
     else if (cursors.right.isDown) {
         //  Move to the right
         player.body.velocity.x = 150;
 
-        player.animations.play('right');
+        //player.body.
+        player.animations.play('walk');
     }
     else {
         //  Stand still
         player.animations.stop();
 
-        player.frame = 4;
+        //player.frame = 4;
     }
 
     //  Allow the player to jump if they are touching the ground.
