d10fe765da75332689a46c5024d5125e603446f3
dickinsonmr1 <dickinsonmr1@gmail.com>
2016-04-29 20:36:48 -0400

    adding tsconfig.json for TypeScript compilation

--- app.js
+++ app.js
@@ -1,19 +1,4 @@
-//class SimpleGame {
-//    constructor() {
-//        this.game = new Phaser.Game(800, 600, Phaser.AUTO, 'content', { preload: this.preload, create: this.create });
-//    }
-//    game: Phaser.Game;
-//    preload() {
-//        this.game.load.image('logo', 'logo.png');
-//    }
-//    create() {
-//        var logo = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'logo');
-//        logo.anchor.setTo(0.5, 0.5);
-//    }
-//}
-//window.onload = () => {
-//    var game = new SimpleGame();
-//};
+/// <reference path="phaser.d.ts" />
 var MyGame = (function () {
     function MyGame() {
         var _this = this;
@@ -588,8 +573,7 @@ var MyGame = (function () {
         this.game = new Phaser.Game(1280, 720, Phaser.CANVAS, 'phaser-example', { preload: this.preload, create: this.create, update: this.update, render: this.render });
     }
     return MyGame;
-})();
+}());
 window.onload = function () {
     var game = new MyGame();
 };
-//# sourceMappingURL=app.js.map
